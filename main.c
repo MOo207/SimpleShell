be4c9d04454f9a0ab6c241b67f7e4e628b61a113
MOo207 <MOo207@github.com>
2020-11-30 21:47:02 +0300

    bank system

--- main.c
+++ main.c
@@ -1,142 +1,263 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include<dirent.h>
-#include <string.h>
-
-void takeInput(char* command){
-reset();
-scanf("%s", command);
-}
-
-char *inputString(FILE* fp, size_t size){
-	char *str;
-	int ch;
-	size_t len = 0;
-	str = realloc(NULL, sizeof(char)*size);
-	if(!str)return str;
-	while(EOF!=(ch=fgetc(fp)) && ch != '\n'){
-		str[len++]=ch;
-		if(len==size){
-			str = realloc(str, sizeof(char)*(size+=16));
-			if(!str)return str;
-		}
-	}
-	str[len++]='\0';
-	return realloc(str, sizeof(char)*len);
-}
-
-DIR *getCurrentDir(){
-    char *currWorkDir, *token;
-    char buffer[PATH_MAX + 1];
-    char *directory;
-    size_t length;
-
-    currWorkDir = getcwd(buffer, PATH_MAX + 1 );
-    token = strrchr(currWorkDir, '/');
-
-    if( currWorkDir == NULL ){
-        printf("Error"); /* You decide here */
-        exit(1);
+//#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#define TRUE    1
+#define FALSE   0
+int  arr[50][3], id, PIN,count=0,check(),entering[100],number[100],N;
+clock_t t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12;
+int max(int number[100]) {
+    int i = 0;
+    int maximum = number[0];
+    for (i = 0; i < N; i++) {
+        if (maximum < number[i])
+            maximum = number[i];
     }
-
-    if (token == NULL) {
-        printf("Error"); /* You decide here */
-        exit(1);
-    }
-
-    length = strlen(token);
-    directory = malloc(length);
-    memcpy(directory, token+1, length);
-
-    return directory;
+    return maximum;
 }
 
-void listFiles()
+void lock(int i) {
+    int j = 0;
+    entering[i] =TRUE;
+    number[i] = 1 + max(number);
+    entering[i] = FALSE;
+    for (j = 0; j < N; j++) {
+        while (entering[j]);
+        while (number[j] != 0 && (number[j] < number[i] || (number[i] == number[j]) && j < i)) {}
+    }
+}
+
+
+void unlock(int i) {
+    number[i] = 0;
+}
+
+
+void* Open(void *integer)
 {
-    DIR *d; // this means the datatype to which d points is of type directory.
-    struct dirent *r; // this's an entery to our directory or can be explined as an access point
-    d=opendir( "./" ); // to open current directory of your current main program directory 'parent folder of your program'
-    red();
-    printf("\n");
-    while((r=readdir(d)) != NULL) //if readdir(d) == null, then it means the directory is not vaild
-    //If successful not null, opendir() returns a pointer to a DIR object d
-    {
-        printf("\t %s \n",r->d_name);
+    int i = (intptr_t) integer;
+    lock(i);
+    printf("\nProcess %d is Entering Critical Section\n",i);
+    t1 = clock();
+    if ((count < 50)){
+    printf(" *******Open_Acouont  (%d)***********\n",i);
+    printf(" Enter your acount\n");
+    scanf("%d", &id);
+    for(int j=0;j<count;j++){
+       if(id==arr[j][0]) {
+           printf("you have acount from befor\n");
+           printf("Process %d is leaving Critical Section\n",i);
+           unlock(i);
+           return 0;
+
+       }
     }
+    arr[count][0]=id;
+    printf("Enter your PIN\n");
+    scanf("%d", &arr[count][1]);
+    printf(" Enter your initial Deposit\n");
+    scanf("%d", &arr[count][2]);
+    count++;
+    t2 = clock();
+    printf("\n TIME IS %f: \n",(double)(t2-t1 )/ CLOCKS_PER_SEC);
+        printf("Process %d is leaving Critical Section\n",i);
+        unlock(i);
+    return 0;}
+     else
+        printf("Bank is full ");
+    printf("Process %d is leaving Critical Section\n",i);
+    unlock(i);
+     return 0;
 }
-
-void red() {
-  printf("\033[1;31m");
-}
-
-void yellow() {
-  printf("\033[1;33m");
-}
-
-void magenta() {
-  printf("\033[0;35m");
-}
-
-void green() {
-  printf("\033[0;32m");
-}
-
-void blue() {
-  printf("\033[0;34m");
-}
-
-void cyan(){
-printf("\033[0;36m");
-}
-
-void boldBlue(){
-printf("\033[1;34m");
-}
-
-void boldMagenta(){
-    printf("\033[1;35m");
-}
-
-void boldCyan(){
-    printf("\033[1;36m");
-}
-
-void boldGreen() {
-  printf("\033[1;32m");
-}
-
-void boldRed() {
-  printf("\033[1;31m");
-}
-
-void boldYellow(){
-printf("\033[01;33m");
-}
-
-void reset() {
-  printf("\033[0m");
-}
-
-int main()
+//-------------------------------------
+void* Close_acount(void *integer)
 {
-    int exit = 0;
-    char* command[20];
+    int i = (intptr_t) integer;
+    lock(i);
+    printf("\nProcess %d is Entering Critical Section\n",i);
+    if (count > 0){
+    t3=clock();
+    printf(" *******Close_Acouont***********\n");
+    int CH=check();
+    if(CH!=-1){
+        printf("Ethe Acount hase closed for big%d and your balance%d\n",arr[CH][0],arr[CH][2]);
+    for (int i=CH;i<count;i++){
+        arr[CH][0]=arr[CH+1][0];
+        arr[CH][1]=arr[CH+1][1];
+        arr[CH][2]=arr[CH+1][2];
+    }
+    count--;
+    t4=clock();
+    printf("\n TIME IS %f: ",(double)(t4-t3 )/ CLOCKS_PER_SEC);
+        printf("Process %d is leaving Critical Section\n",i);
+        unlock(i);
+    return 0;}
+    else
+        t4=clock();
+    printf("\n TIME IS %f: ",(double)(t4-t3 )/ CLOCKS_PER_SEC);
+        printf("Process %d is leaving Critical Section\n",i);
+        unlock(i);
+    return 0;
+    }
+    else{
+    printf("Bank IS EMPTY");
+    printf("Process %d is leaving Critical Section\n",i);
+    unlock(i);
+    return 0;}
+}
+//------------------------------------------
+void* Withdraw(void *integer)
+{
+    int i = (intptr_t) integer;
+    lock(i);
+    printf("\nProcess %d is Entering Critical Section\n",i);
+    t5=clock();
+    int my = 0;
+    int CH = check();
+    if (CH != -1)
+    printf("\n Enter Withdraw Amunt: ");
+    printf("\n Should be integer number: ");
+    scanf("%d", &my);
+    if(my<=arr[CH][2]){
+        arr[CH][2] = arr[CH][2] - my;
+        t6 =clock();
+        printf("\n TIME IS %f: ",(double)(t6-t5 )/ CLOCKS_PER_SEC);
+        printf("Process %d is leaving Critical Section\n",i);
+        unlock(i);
+    return 0;}
+    else
+        printf("\n you can't becouse your Withdraw greten than your balance: ");
+    t6 =clock();
+    printf("\n TIME IS %f: ",(double)(t6-t5 )/ CLOCKS_PER_SEC);
+    printf("Process %d is leaving Critical Section\n",i);
+    unlock(i);
+    return 0;
+}
+//---------------------------------------
+void* Depooseit(void *integer)
+{
+    int i = (intptr_t) integer;
+    lock(i);
+    printf("\nProcess %d is Entering Critical Section\n",i);
+    t7 =clock();
+    int my = 0;
+    int CH = check();
+    if (CH != -1){
+        printf(" Enter Deposit Amunt: ");
+        scanf("%d", &my);
+        arr[CH][2] +=  my;
 
-    while(exit != 1){
-        //DIR *dir = getCurrentDir();
-        green();
-        printf("[Guest ]$ ");
-        reset();
-        takeInput(command);
-        if(strcmp(command, "ls") == 0){
-            listFiles();
-        } else if(strcmp(command, "exit") == 0){
-            return 0;
-        } else if(strcmp(command, "help") == 0){
-        yellow();
-        printf("ls for lising dir and files\nexit to go out\nhelp for more info\n");
+        t8 =clock();
+        printf("\n TIME IS %f: ",(double)(t8-t7 )/ CLOCKS_PER_SEC); }
+
+    printf("Process %d is leaving Critical Section\n",i);
+    unlock(i);
+    return 0;
+
+}
+//-----------------------------------------------------
+void* inquire(void *integer)
+{
+    int i = (intptr_t) integer;
+    lock(i);
+    printf("\nProcess %d is Entering Critical Section\n",i);
+    t9 =clock();
+    int CH = check();
+    if (CH!=-1){
+        printf("\n your balence is %d: ",arr[CH][2]);
+        t10 =clock();
+        printf("\n TIME IS %f: ",(double)(t10-t9 )/ CLOCKS_PER_SEC); }
+    printf("Process %d is leaving Critical Section\n",i);
+    unlock(i);
+         return 0;
+}
+void* servi()
+{
+    t11=clock();
+    //sleep(1);
+    printf("1.Open_Acouont\n");
+    //sleep(1);
+    printf("2.Close-acount\n");
+    //sleep(1);
+    printf("3.Depooseit\n");
+    //sleep(1);
+    printf("4.Withdraw\n");
+    //sleep(1);
+    printf("5.inquire\n");
+    //sleep(1);
+    printf("6.exit\n");
+
+    t12 =clock();
+    printf("\n TIME IS %f: ",(double)(t12-t11 )/ CLOCKS_PER_SEC);return 0;
+}
+//----------------------------------------------------
+int main() {
+
+    printf("Enter Number of Process\n");
+    scanf("%d",&N);
+    int n;
+    pthread_t g1[N];
+    pthread_t g2[N];
+    pthread_t g3[N];
+    pthread_t g4[N];
+    pthread_t g5[N];
+    pthread_t g6;
+
+
+    while (1) {
+        pthread_create(&g6,NULL,servi,NULL);
+        pthread_join(g6,NULL);
+        printf("\nENTER YOUR CHOICE\n");
+        scanf("%d", &n);
+        switch (n) {
+            case 1:
+               for(int i=0;i<N;i++){
+                    pthread_create(&g1[i],NULL,Open,(void *)(intptr_t)i);
+                    pthread_join(g1[i],NULL);}
+
+                break;
+            case 2:
+                for(int i=0;i<N;i++){
+                        pthread_create(&g2[i],NULL,Close_acount,(void *)(intptr_t)i);
+                pthread_join(g2[i],NULL);}
+                break;
+
+            case 3:
+                for(int i=0;i<N;i++){
+                    pthread_create(&g3[i],NULL,Depooseit, (void *)(intptr_t)i);
+                pthread_join(g3[i],NULL);}
+                break;
+            case 4:
+                for(int i=0;i<N;i++){
+                    pthread_create(&g4[i],NULL,Withdraw, (void *)(intptr_t)i);
+                pthread_join(g4[i],NULL);}
+                break;
+            case 5:
+                for(int i=0;i<N;i++){
+                    pthread_create(&g5[i],NULL,inquire, (void *)(intptr_t)i);
+                pthread_join(g5[i],NULL);}
+                break;
+            case 6:
+                printf("thank you for use our banking\n");
+
+               return 0;
+
+
         }
-
     }
 }
+//------------------------------------------------------------------------
+int check(){
+    printf(" Enter your acount\n");
+    scanf("%d",&id);
 
+    printf(" Enter your PIN\n");
+    scanf("%d",&PIN);
+    for(int i=0;i<count;i++){
+    if (id==arr[i][0]&&PIN==arr[i][1]){
+        return i;}}
+
+    printf(" invalid ID or PIN\n");
+            return -1;
+
+    }
